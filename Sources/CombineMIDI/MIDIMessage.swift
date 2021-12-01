72e04bf3611d80cf3676c31505e0fbc49febb6a2
Fabian Canas <fcanas@gmail.com>
2021-11-30 23:39:58 -0500

    Fix MIDIMessage representation of MIDI 1 spec
    A MIDI message be shorter than 3 bytes

--- Sources/CombineMIDI/MIDIMessage.swift
+++ Sources/CombineMIDI/MIDIMessage.swift
@@ -1,5 +1,55 @@
 /// Typed MIDI message interpretation.
-public struct MIDIMessage {
+public enum MIDIMessage {
+    
+    public struct Note {
+        public var channel: UInt8
+        public var noteNumber: UInt8
+        public var velocity: UInt8
+    }
+    
+    public struct PolyphonicAftertouch {
+        public var channel: UInt8
+        public var noteNumber: UInt8
+        public var pressure: UInt8
+    }
+    
+    public struct ProgramChange {
+        public var channel: UInt8
+        public var program: UInt8
+    }
+    
+    public struct ChannelAftertouch {
+        public var channel: UInt8
+        public var pressureValue: UInt8
+    }
+    
+    public struct PitchBend {
+        public var channel: UInt8
+        public var lsb: UInt8
+        public var msb: UInt8
+    }
+    
+    public struct ControlChange {
+        public var channel: UInt8
+        public var control: UInt7
+        public var controlValue: UInt8
+    }
+    
+    case noteOff(Note)
+    case noteOn(Note)
+    case afterTouch(PolyphonicAftertouch)
+    case controlChange(ControlChange)
+    case programChange(ProgramChange)
+    case channelPressure(ChannelAftertouch)
+    case pitchBendChange(PitchBend)
+    case tuneRequest
+    case timingClock
+    case start
+    case stop
+    case `continue`
+    case activeSensing
+    case systemReset
+    
     /// Kind of the message.
     public enum Status: UInt8 {
         /// Note Off event.
@@ -36,19 +86,19 @@ public struct MIDIMessage {
         /// a change in the pitch bender (wheel or lever, typically).
         /// The pitch bender is measured by a fourteen bit value.
         case pitchBendChange = 0xE0
+        
+        case tuneRequest = 0xF6
+        case timingClock = 0xF8
+        case start = 0xFA
+        case `continue` = 0xFB
+        case stop = 0xFC
+        case activeSensing = 0xFE
+        case systemReset = 0xFF
     }
 
-    /// Kind of the message.
-    public let status: Status
-    /// Channel of the message. Between 0-15.
-    public let channel: UInt8
-    /// First data byte. Usually a key number (note or controller). Between 0-127.
-    public let data1: UInt8
-    /// Second data byte. Usually a value (pressure, velocity or program number). Between 0-127.
-    public let data2: UInt8
-
     /// All Notes Off. When an All Notes Off is received, all oscillators will turn off.
-    static let allNotesOff: MIDIMessage = MIDIMessage(status: .controlChange, channel: 0, data1: 123, data2: 0)
+    static let allNotesOff: MIDIMessage = .controlChange(ControlChange(channel: 0, control: 123, controlValue: 0))
+        
 
     /// Initializes new message manually with all the parameters.
     /// - Parameters:
@@ -56,24 +106,49 @@ public struct MIDIMessage {
     ///   - channel: Channel of the message. Between 0-15.
     ///   - data1: First data byte. Usually a key number (note or controller). Between 0-127.
     ///   - data2: Second data bytes. Usually a value (pressure, velocity or program number). Between 0-127.
-    public init(status: Status = .noteOn, channel: UInt8 = 0, data1: UInt8 = 0, data2: UInt8 = 0) {
-        self.status = status
-        self.channel = channel
-        self.data1 = data1
-        self.data2 = data2
-    }
 
     /// Initializes new message automatically by parsing an array of bytes (usually from a MIDI packet).
     /// - Parameters:
     ///   - bytes: Array of bytes. At least three bytes must be supplied to create strongly-typed
     ///   MIDI message.
     public init?(bytes: [UInt8]) {
-        guard bytes.count >= 3, let status = Status(rawValue: bytes[0] & 0xF0) else {
+        guard bytes.count >= 2, let status = Status(rawValue: bytes[0] & 0xF0) else {
             return nil
         }
-        self.status = status
-        self.channel = bytes[0] & 0x0F
-        self.data1 = bytes[1]
-        self.data2 = bytes[2]
+        
+        let channel: UInt8 = bytes[0] & 0x0F
+        let data1: UInt8 = bytes.count > 1 ? bytes[1] : 0
+        let data2: UInt8 = bytes.count > 2 ? bytes[2] : 0
+        
+        switch status {
+        case .noteOff:
+            self = .noteOff(Note(channel: channel, noteNumber: data1, velocity: data2))
+        case .noteOn:
+            self = .noteOn(Note(channel: channel, noteNumber: data1, velocity: data2))
+        case .aftertouch:
+            self = .afterTouch(PolyphonicAftertouch(channel: channel, noteNumber: data1, pressure: data2))
+        case .controlChange:
+            self = .controlChange(ControlChange(channel: channel, control: UInt7(data1), controlValue: data2))
+        case .programChange:
+            self = .programChange(ProgramChange(channel: channel, program: data1))
+        case .channelPressure:
+            self = .channelPressure(ChannelAftertouch(channel: channel, pressureValue: data1))
+        case .pitchBendChange:
+            self = .pitchBendChange(PitchBend(channel: channel, lsb: data1, msb: data2))
+        case .tuneRequest:
+            self = .tuneRequest
+        case .timingClock:
+            self = .timingClock
+        case .start:
+            self = .start
+        case .continue:
+            self = .continue
+        case .stop:
+            self = .stop
+        case .activeSensing:
+            self = .activeSensing
+        case .systemReset:
+            self = .systemReset
+        }
     }
 }
